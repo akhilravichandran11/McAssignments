9b3a0dd5071229f2facf11a2225c3bf2ed2964a2
chaitanya jetti <chaitanya@chaitanyas-MacBook-Pro.local>
2017-04-14 16:11:38 -0700

    Fixed webview.

--- MCAssignment3/app/src/main/java/com/mobilecomputing/chaitanya/mcassignment3/webview.java
+++ MCAssignment3/app/src/main/java/com/mobilecomputing/chaitanya/mcassignment3/webview.java
@@ -1,22 +1,12 @@
 package com.mobilecomputing.chaitanya.mcassignment3;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.webkit.JavascriptInterface;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
-
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class webview extends AppCompatActivity {
 
@@ -27,7 +17,6 @@ public class webview extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_webview);
 
-        // Prepare webview: add zoom controls and start zoomed out
         webview = (WebView) findViewById(R.id.webview);
         final WebSettings webSettings = webview.getSettings();
         webSettings.setJavaScriptEnabled(true);
@@ -42,21 +31,17 @@ public class webview extends AppCompatActivity {
             public void onPageFinished(WebView view, String url) {
 
                 int[][] walking = {{5, 4, 2}, {9, 6, 1}, {2, 7, 3}, {4, 5, 4}, {6, 8, 1}, {3, 4, 0}};
-                String text1 = Arrays.toString(walking);
+                String text1 = Arrays.deepToString(walking);
 
                 int[][] running = {{6, 2, 5}, {0, 4, 9}, {3, 5, 9}, {6, 9, 1}, {1, 9, 2}};
-                String text2 = Arrays.toString(running);
+                String text2 = Arrays.deepToString(running);
 
                 int[][] eating = {{2, 2, 6}, {5, 1, 2}, {9, 9, 7}, {6, 9, 9}, {8, 4, 3}, {4, 1, 7}};
-                String text3 = Arrays.toString(eating);
+                String text3 = Arrays.deepToString(eating);
 
-                Log.d("Walking", text1);
-                Log.d("Running", text1);
-                Log.d("Eating", text1);
-                webview.loadUrl("javascript:showGraph()");
+                webview.loadUrl("javascript:showGraph(" + text1 + ", " + text2 + ", " +text3 + ")");
             }
         });
-        // Load base html from the assets directory
         webview.loadUrl("file:///android_asset/html/graph.html");
 
     }
